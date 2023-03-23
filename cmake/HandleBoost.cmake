61d23ce6029f836caa7d8f1052c7c7445b3e2be2
ShuangLiu <liushuang.work@gmail.com>
2023-03-23 12:14:34 +0000

    boost 1.82

--- cmake/HandleBoost.cmake
+++ cmake/HandleBoost.cmake
@@ -1,15 +1,17 @@
 ###############################################################################
 # Find boost
 
-find_package(boost_serialization 1.81 REQUIRED)
-find_package(boost_system 1.81 REQUIRED)
-find_package(boost_filesystem 1.81 REQUIRED)
-find_package(boost_thread 1.81 REQUIRED)
-find_package(boost_date_time 1.81 REQUIRED)
-find_package(boost_regex 1.81 REQUIRED)
-find_package(boost_concept_check 1.81 REQUIRED)
-find_package(boost_timer 1.81 REQUIRED)
-find_package(boost_chrono 1.81 REQUIRED)
+find_package(Boost REQUIRED COMPONENTS 
+  serialization
+  system
+  filesystem
+  thread
+  date_time
+  regex
+  concept_check
+  timer
+  chrono
+  )
  
 option(GTSAM_DISABLE_NEW_TIMERS "Disables using Boost.chrono for timing" OFF)
 # Allow for not using the timer libraries on boost < 1.48 (GTSAM timing code falls back to old timer library)
